9bc361d7b61fd8f48b0e7321de755437e6a9f339
Shehryar <shehryar11w@gmail.com>
2026-03-31 12:46:44 +0500

    vercel fixes

--- src/main.ts
+++ src/main.ts
@@ -16,10 +16,7 @@ async function bootstrap() {
 	const config = new DocumentBuilder()
 		.setTitle('Crescent API')
 		.setDescription('API documentation for Crescent project')
-		.setVersion('1.0'
-
-			
-		)
+		.setVersion('1.0')
 		.build();
 	const document = SwaggerModule.createDocument(app, config);
 	SwaggerModule.setup('api', app, document);
