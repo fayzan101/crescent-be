a22df43ff689a51999334433054cd45fb8f80cca
GitHub Action <action@github.com>
2026-04-02 11:30:20 +0000

    Auto sync from main repo

---
+++ target/target/target/target/dist/modules/clients/clients.controller.d.ts
@@ -0,0 +1,14 @@
+import { ClientsService } from './clients.service';
+import { CreateClientDto } from './dto/create-client.dto';
+export declare class ClientsController {
+    private readonly clientsService;
+    constructor(clientsService: ClientsService);
+    createClient(createClientDto: CreateClientDto): Promise<{
+        name: string;
+        id: number;
+        email: string;
+        cnic: string;
+        irNo: string;
+        phone: string;
+    }>;
+}
